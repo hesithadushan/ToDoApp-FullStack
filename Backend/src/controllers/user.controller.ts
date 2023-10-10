8ea40e556a4425be28db0d98ae0d81ab9147c9ef
hesithadushan <135688927+hesithadushan@users.noreply.github.com>
2023-10-10 14:24:28 +0530

    commit 5

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -1,6 +1,18 @@
 import {Request, Response } from "express";
-import bcrypt from 'bcrypt'
-import User from "../models/user.model"
+import bcrypt from 'bcrypt';
+import User from "../models/user.model";
+import jwt from "jsonwebtoken";
+import {IUser} from "../types"
+import { Types } from "mongoose";
+
+
+const getUserToken = (_id: string | Types.ObjectId) => {
+    const authenticatedUserToken = jwt.sign({ _id }, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+    return authenticatedUserToken;
+  };
+
 
 export const createUser = async (request: Request, response: Response) => {
     try{
@@ -27,6 +39,34 @@ export const createUser = async (request: Request, response: Response) => {
         throw error
     }
     
-}
+};
 
+export const loginUser = async (request: Request, response: Response) =>{
+    try {
+        const{email,password}:IUser = request.body 
+        const existingUser = await User.findOne({email})
 
+        if(!existingUser){
+            return response.status(409).send({message:"User dosen't exist"})
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password,existingUser.password)
+
+        if(isPasswordMatched){
+            const token = getUserToken((existingUser)._id)
+            return response.send({
+                token,
+                user:{
+                    email:existingUser.email,
+                    name: existingUser.name,
+                },
+            })
+        }else{
+            return response.status(400).send({message:"Incorrect Password"})
+        }
+
+    } catch (error) {
+        console.log("Cannot login user", error)
+        throw error
+    }
+};
